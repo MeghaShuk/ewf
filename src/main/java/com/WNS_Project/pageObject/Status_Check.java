fdc4eaa63ca59e49419f77f2bd28bdc9525eafae
unknown <megha.shukla@zeeve.io>
2024-12-04 19:39:49 +0530

    file Modifications Done

--- src/main/java/com/WNS_Project/pageObject/Status_Check.java
+++ src/main/java/com/WNS_Project/pageObject/Status_Check.java
@@ -85,10 +85,10 @@ public class Status_Check extends BaseClass {
 	}
 
 	public void SeedPhrase_Button() throws InterruptedException {
-		// wait.until(ExpectedConditions.visibilityOf(seed_phrase_click));
+		wait.until(ExpectedConditions.visibilityOf(seed_phrase_click));
 		Thread.sleep(5000);
 		Actions action = new Actions(driver);
-		action.moveToElement(seed_phrase_click).click();
+		action.moveToElement(seed_phrase_click).click(seed_phrase_click);
 	}
 
 	public void Create_Button() {
